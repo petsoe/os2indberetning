58a01bc72cc315677f87b5e0b61068c8aad537dc
Martin Hvidberg <mhn@miracle.dk>
2016-08-10 15:48:14 +0200

    Project checked in

---
+++ SixtyDaysNotifier/NotifierService.cs
@@ -0,0 +1,27 @@
+﻿using Core.DomainModel;
+using Core.DomainServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixtyDaysNotifier
+{
+    class NotifierService
+    {
+        private IGenericRepository<DriveReport> reportRepo;
+
+        public NotifierService(IGenericRepository<DriveReport> reportRepo)
+        {
+            this.reportRepo = reportRepo;
+        }
+
+        internal void RunNotifierService()
+        {
+            var routeGeometry = reportRepo.AsQueryable().Where(r => r.Purpose.Equals("MEGA TEST") );
+            
+        }
+
+    }
+}
---
+++ SixtyDaysNotifier/Program.cs
@@ -0,0 +1,16 @@
+﻿using Core.ApplicationServices;
+using Ninject;
+
+namespace SixtyDaysNotifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var service = NinjectWebKernel.CreateKernel().Get<NotifierService>();
+            service.RunNotifierService();
+            
+        }
+    }
+}
