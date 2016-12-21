7a12bf60e0139c46c952fb6ff9ded84a02e705e9
Martin Hvidberg <mhn@miracle.dk>
2016-12-21 08:55:21 +0100

    Fixed issue with report on wrong employment

--- Presentation.Web/App/Driving/DrivingController.js
+++ Presentation.Web/App/Driving/DrivingController.js
@@ -170,7 +170,10 @@
             $scope.DriveReport.FourKmRule = {};
             $scope.DriveReport.FourKmRule.Value = $scope.currentUser.DistanceFromHomeToBorder.toString().replace(".", ",");
             
-           
+           if(report.EmploymentId != null){
+             // Set default DriveReport Position to position from previous report
+            $scope.DriveReport.Position = report.EmploymentId;
+            }
           
             // Select position in dropdown.
             $scope.container.PositionDropDown.select(function (item) {
